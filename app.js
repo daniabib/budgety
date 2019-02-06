e709cc8c1be8d557f7e947b4834ffbe4090b2f28
Daniel Abib <danielsabib@gmail.com>
2019-02-06 20:08:46 -0200

    Implemented calculateBudget method

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    }
+
     // Data structure to receive data
     var data = {
         allItems: {
@@ -23,7 +31,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -50,6 +60,29 @@ var budgetController = (function() {
             // Return the new element
             return newItem;
             
+        },
+
+        calculateBudget: function() {
+
+            // Calculate total income and expanses
+            calculateTotal('exp');
+            calculateTotal('exp')
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percentage of income that we spent
+            data.percentage = Math.rount(((data.totals.exp / data.totals.inc)) * 100);
+
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function() {
@@ -145,11 +178,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var updateBudget = function() {
+
         // 1. Calculate budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget;
 
         // 3. Display the budget on the UI
+        console.log(budget);
 
     }
 
